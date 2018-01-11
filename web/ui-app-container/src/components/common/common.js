98bed27222cdea3c00ac55b396a85c0e3960747b
Varun Hegde <1hvarun@gmail.com>
2018-01-11 07:20:03 +0530

    seperated translation file from the commons

--- web/ui-app-container/src/components/common/common.js
+++ web/ui-app-container/src/components/common/common.js
@@ -1,27 +1,5 @@
-var localization_EN_Data = {
-  'deshboard.title': 'My Tasks',
-  'pgr.msg.createaccount.avail.onlineservices': ' Create an account to avail online services',
-  'pgr.lbl.apply.service': 'Apply For a Service',
-  'pgr.lbl.apply.servicetag': 'Apply Service',
-  'pgr.msg.complaintstatus.application': 'Know the status of your application!',
-  'pgr.msg.complaintstatus.certificate': 'Certificate Status',
-  'core.lbl.varify': 'Verify',
-  'pgr.lbl.applicationnumber': ' Application Number',
-  'pgr.lbl.certificateNumber': 'Certificate Number',
-};
+import { localization_EN_Data, localization_MR_Data } from './translation';
 
-var localization_MR_Data = {
-  'deshboard.title': 'My Tasks',
-  'ui.framework.required': 'आवश्यक',
-  'core.lbl.varify': 'पडताळा',
-  'ui.framework.patternMessage': ' वरील नमुने जुळवा',
-  'pgr.lbl.apply.service': ' तक्रार नोंदवा',
-  'pgr.lbl.apply.servicetag': ' तक्रार नोंदवा',
-  'pgr.msg.complaintstatus.application': ' कधीही आपल्या तक्रारीची स्थिती जाणून घ्या!',
-  'pgr.msg.complaintstatus.certificate': 'प्रमाणपत्र स्थिती',
-  'pgr.lbl.applicationnumber': ' तक्रार क्रमांक',
-  'pgr.lbl.certificateNumber': 'प्रमाणपत्र क्रमांक',
-};
 export function translate(locale_text) {
   if (locale_text && localStorage.getItem('lang_response')) {
     var langresult = JSON.parse(localStorage.getItem('lang_response')).filter(function(obj) {
---
+++ web/ui-app-container/src/components/common/translation.js
@@ -0,0 +1,24 @@
+export const localization_EN_Data = {
+  'deshboard.title': 'My Tasks',
+  'pgr.msg.createaccount.avail.onlineservices': ' Create an account to avail online services',
+  'pgr.lbl.apply.service': 'Apply For a Service',
+  'pgr.lbl.apply.servicetag': 'Apply Service',
+  'pgr.msg.complaintstatus.application': 'Know the status of your application!',
+  'pgr.msg.complaintstatus.certificate': 'Certificate Status',
+  'core.lbl.varify': 'Verify',
+  'pgr.lbl.applicationnumber': ' Application Number',
+  'pgr.lbl.certificateNumber': 'Certificate Number',
+};
+
+export const localization_MR_Data = {
+  'deshboard.title': 'My Tasks',
+  'ui.framework.required': 'आवश्यक',
+  'core.lbl.varify': 'पडताळा',
+  'ui.framework.patternMessage': ' वरील नमुने जुळवा',
+  'pgr.lbl.apply.service': ' तक्रार नोंदवा',
+  'pgr.lbl.apply.servicetag': ' तक्रार नोंदवा',
+  'pgr.msg.complaintstatus.application': ' कधीही आपल्या तक्रारीची स्थिती जाणून घ्या!',
+  'pgr.msg.complaintstatus.certificate': 'प्रमाणपत्र स्थिती',
+  'pgr.lbl.applicationnumber': ' तक्रार क्रमांक',
+  'pgr.lbl.certificateNumber': 'प्रमाणपत्र क्रमांक',
+};
