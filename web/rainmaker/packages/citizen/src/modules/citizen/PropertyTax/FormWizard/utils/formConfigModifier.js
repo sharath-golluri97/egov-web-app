df915f4dd9a67651f345380cc4ffa32b53d750ae
Girish Patro <girish.prasad@goodworklabs.com>
2018-07-30 19:36:10 +0530

    Integrate with MDMS
    Integrated with MDMS

---
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/FormWizard/utils/formConfigModifier.js
@@ -0,0 +1,29 @@
+const subCategoriesInOwnersType = ["INDIVIDUAL"]
+
+const formOwnerDropdown = category => {
+  const { name, code } = category
+  return {
+    label: name,
+    value: code,
+  }
+}
+
+export const getOwnerDetails = state => {
+  const { OwnerShipCategory, SubOwnerShipCategory } = JSON.parse(JSON.stringify(state.common.generalMDMSDataById))
+  const ownerShipdropDown = []
+  debugger
+  Object.keys(OwnerShipCategory).forEach((category) => {
+    const categoryCode = OwnerShipCategory[category].code
+    if (subCategoriesInOwnersType.indexOf(categoryCode) !== -1) {
+      Object.keys(SubOwnerShipCategory)
+        .filter(subCategory => categoryCode === SubOwnerShipCategory[subCategory].ownerShipCategory)
+        .forEach(linkedCategory => {
+          ownerShipdropDown.push(formOwnerDropdown(SubOwnerShipCategory[linkedCategory]))
+        })
+    } else {
+      ownerShipdropDown.push(formOwnerDropdown(OwnerShipCategory[category]))
+    }
+    console.log("ownerShipdropDown======================================================================================================>", ownerShipdropDown)
+  })
+  return ownerShipdropDown
+}
